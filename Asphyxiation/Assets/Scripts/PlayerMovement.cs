7867166f3a0d25718aac08ca0108c9675c02a3a4
Acerii <r.m.acevedo1130@gmail.com>
2023-12-26 09:15:03 -0500

    First Person Movement Added

---
+++ Asphyxiation/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    public Transform orientation;
+
+    float horizontalInput;
+
+    float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputHandler();
+        MovePlayer();
+    }
+
+    private void InputHandler()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force); 
+    }
+}
